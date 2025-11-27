8f10d1a503a4b6f87f5c4da00f8e93de8901070a
a7medS3yed <sa8072750@gmail.com>
2025-11-27 18:51:14 +0200

    Products Entities

---
+++ ApplicationCoreLayer/ECommerce.Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+        public decimal Price { get; set; }
+
+        #region RelationShip
+
+        public int ProductTypeId { get; set; }
+        public ProductType ProductType { get; set; } = default!;
+        public int ProductBrandId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = default!;
+
+        #endregion
+
+    }
+}
