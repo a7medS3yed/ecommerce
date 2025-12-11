5868a47a43783d9fa8d3b45da84cd64c69247be1
a7medS3yed <sa8072750@gmail.com>
2025-12-11 23:54:46 +0200

    Authentication Endpoint

---
+++ PresentaionLayer/ECommerce.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Service.Abstraction.Identity;
+using ECommerce.Shared.Dtos.Identitys;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Presentation.Controllers
+{
+    public class AuthenticationController(IAuthentication authentication) : ApiBaseController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(UserRegisterDto registerDto)
+        {
+            var result = await authentication.RegisterAsync(registerDto);
+
+            return HandleProblem(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(UserLoginDto loginDto)
+        {
+            var result = await authentication.LoginAsync(loginDto);
+            return HandleProblem(result);
+        }
+    }
+}
